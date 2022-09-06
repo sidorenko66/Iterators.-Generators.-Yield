b2286945fdfec713dfa7eed6168f51043a40c2f4
sidorenko66 <105962938+sidorenko66@users.noreply.github.com>
2022-09-06 21:49:53 +0500

    fix

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class FlatIterator:
 
 
 nested_list = [
-    ['a', 'b', 'c', ],
+    ['a', 'b', 'c'],
     ['d', 'e', 'f', 'h', False],
     [1, 2, None],
 ]
@@ -53,7 +53,7 @@ def flat_generator(list1):
 nested_list = [
     ['a', 'b', 'c'],
     ['d', 'e', 'f'],
-    [1, 2, None, ],
+    [1, 2, None],
 ]
 for item in flat_generator(nested_list):
     print(item)
